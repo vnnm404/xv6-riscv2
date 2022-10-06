126f20b876eaa67f4481f082809111aa7225e4ff
vnnm <vennam404@gmail.com>
2022-10-06 18:33:39 +0530

    [add] source for random.c implementation

--- kernel/random.c
+++ kernel/random.c
@@ -2,6 +2,7 @@
 
 #define RAND_MAX    32767
 
+// src (https://stackoverflow.com/questions/4768180/rand-implementation)
 int rand(uint64 seed) // RAND_MAX assumed to be 32767
 {
   seed = seed * 1103515245 + 12345;
